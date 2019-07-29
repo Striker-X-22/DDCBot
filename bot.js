3997e228def638d5da093f77f10ee3230f59caeb
Striker-X-22 <52842699+Striker-X-22@users.noreply.github.com>
2019-07-28 19:56:05 -0700

    Spam prevention added. Removed pinging by request.
    Encountered a spam bug on 1st event announce after bot had been on for days prior. I'm not certain what caused it yet, but I believe it wouldn't be possible to spam for very long before it corrects itself, so I'm giving this a full day to try to make sure, while not interfering with the next announcement.
    
    Removed pinging time attack role as one mod thought it would be too excessive 3x a week.
    
    Pushed back ddc starting date for lack of approval from staff yet. Small other inconsequential changes/fixes.

--- bot.js
+++ bot.js
@@ -29,7 +29,7 @@ bot.login(token);
 const prefix = data.prefix;
 const mainChID = data.mainRedoutChID; // Where announcements are made.
 const botCmdChID = data.botCmdChID;
-const TimeAttackRole = data.TimeAttackRoleID;
+const TimeAttackRole = data.TimeAttackRoleID; // Unused for now, was planned on being used for pinging.
 const DDCStartStr = data.DDCStart;
 // All events for DDC season 1. The durations will all be 2 day intervals, skipping Saturday: Sun-Mon, Tue-Wed, Thur-Fri. Calculated from a spreadsheet. Assumes starting on Sunday.
 const DDCEvents = data.DDCEvents; // Technically only the ref is const.
@@ -41,6 +41,9 @@ const msInMinute = 1000*60;
 let timer1 = -1; // These timers are used for tracking the 3 different times a week events happen for DDC. When assigned by SetTimeout(), they are given the timer ID.
 let timer2 = -1;
 let timer3 = -1;
+let timerSpamPrevent = -1; // Encountered a spam bug on 1st event announce after bot had been on for days prior. I'm not certain what caused it yet, but I believe it wouldn't be possible to spam for very long before it corrects itself, so I'm giving this a full day to try to make sure, while not interfering with the next announcement.
+let okToSend = true; // Used with above timerSpamPrevent.
+let debug = false; // *** debug mode. Just a switch to check for to test whatever. Turn off for release.
 
 bot.once('ready', function (evt) {
 
@@ -80,7 +83,7 @@ bot.once('ready', function (evt) {
 					console.log("<> Calc timeToNext: ddcStart.getTime()="+ddcStart.getTime()+", DDCEvents[i+1].day="+DDCEvents[i+1].day+", DDCEvents[i+1].day*msInDay="+DDCEvents[i+1].day*msInDay+", startupTime.getTime()="+startupTime.getTime()+".");
 					let timeToNext = ddcStart.getTime() + DDCEvents[i+1].day*msInDay - startupTime.getTime(); // Find time until event i+1 since startupTime.
 					let day = DDCEvents[i+1].day % 7; // Which day. 0 = Sunday.
-					console.log("<> Re-Announce trying to set timer. day: "+day+", timeToNext: "+timeToNext+", updateOneTimer: "+"true"+".");
+					console.log("<> Re-Announce trying to set timer. day: "+day+", timeToNext: "+timeToNext+", usedAnnounce: "+"false"+".");
 					SetTimers(day, timeToNext);
 				} else { // Last event already. Do nothing, I guess.
 				}
@@ -162,9 +165,13 @@ function Announce() { // Ended up being pretty much a copy/paste of startup re-a
 					timer3 = 0;
 				}
 				if (timeLeft >= 30*msInMinute) { // Hopefully timing is never off by more than half an hour. If it is, it should simply re-announce the old event then announce the new one at around the right time.
-					// old format: "**Double Day Challenge** is now _**-Surface Sprint: IV Time Attack-**_ for 48 hours (Week 20)." Have to use <@xxxxxxxx> for users, but with @& for roles.
-					// The actual * message: *
-					mainCh.send("_Double Day Challenge_ is now:       _(Week " + Math.floor(1 + DDCEvents[i].day/7) + ")_\n**[_ -" + DDCEvents[i].title + "- _]**   for 48 hours. Have fun! <@&"+TimeAttackRole+">" + lastEvent);
+					if (okToSend) {
+						// old format: "**Double Day Challenge** is now _**-Surface Sprint: IV Time Attack-**_ for 48 hours (Week 20)." Have to use <@xxxxxxxx> for users, but with @& for roles.
+						// The actual * message: *
+						mainCh.send("_Double Day Challenge_ is now:       _(Week " + Math.floor(1 + DDCEvents[i].day/7) + ")_\n**[_ -" + DDCEvents[i].title + "- _]**   for 48 hours. Have fun!" + lastEvent);// <@&"+TimeAttackRole+">" + lastEvent); // No ping for now.
+					} else {
+						console.log("<> *Error*: Tried to announce while okToSend was false. Just setting timers again."); 
+					}
 				} else {
 					// Seems to be early in timing, trying to announce the same event again before actual time is completely up and next event is out. Just proceed and still set timers.
 					console.log("<> *Error*: Tried to announce early. Just setting timers again.");
@@ -172,10 +179,10 @@ function Announce() { // Ended up being pretty much a copy/paste of startup re-a
 				// Set timers for next 3 events (since they vary every 3).
 				if (lastEvent == "") { // Not last event.
 					// Find which day it is:
-					console.log("<> Calc timeToNext: ddcStart.getTime()="+ddcStart.getTime()+", DDCEvents[i+1].day="+DDCEvents[i+1].day+", DDCEvents[i+1].day*msInDay="+DDCEvents[i+1].day*msInDay+", startupTime.getTime()="+curTime.getTime()+".");
+					console.log("<> Calc timeToNext: ddcStart.getTime()="+ddcStart.getTime()+", DDCEvents[i+1].day="+DDCEvents[i+1].day+", DDCEvents[i+1].day*msInDay="+DDCEvents[i+1].day*msInDay+", curTime.getTime()="+curTime.getTime()+".");
 					let timeToNext = ddcStart.getTime() + DDCEvents[i+1].day*msInDay - curTime.getTime(); // Find time until event i+1 since curTime.
 					let day = DDCEvents[i+1].day % 7; // Which day. 0 = Sunday.
-					console.log("<> Announce trying to set timer. day: "+day+", timeToNext: "+timeToNext+", updateOneTimer: "+"true"+".");
+					console.log("<> Announce trying to set timer. day: "+day+", timeToNext: "+timeToNext+", usedAnnounce: "+"true"+".");
 					SetTimers(day, timeToNext, true); // Not using last optional term currently. Just setting all timers instead.
 				} else { // Last event already. Do nothing, I guess.
 				}
@@ -251,29 +258,39 @@ function AskEvent(ch) { // User asks the current event and time left. ch is chan
 } // AskEvent()
 
 
-function SetTimers(dayNumID, timeToNextInMS, updateOneTimer=false) {
-	console.log("<> Timer(s) being set. day = "+dayNumID+", timeToNext = "+timeToNextInMS+", updateOneTimer = "+updateOneTimer+"." );
-	// Probably no particular reason not to just set all 3 timers each time every announcement. Thus, updateOneTimer is unused.
-	let debug = "";
+function SetTimers(dayNumID, timeToNextInMS, usedAnnounce=false) {
+	console.log("<> Timer(s) being set. day = "+dayNumID+", timeToNext = "+timeToNextInMS+", usedAnnounce = "+usedAnnounce+"." );
+	// I set all 3 timers again to try to make sure they stay accurate. 
+	let debug1 = "";
+	
+	// Spam prevention. Patch for a bug found that isn't fixed yet, regarding very 1st event announcement after bot has been online for days. This will put a day limit and hope it sorts itself out (which in theory it should).
+	if (usedAnnounce){
+		timerSpamPrevent = setTimeout(SpamPrevent, msInDay-msInMinute);
+		okToSend = false; // Set to true after timerSpamPrevent is up.	
+	}
+	
 	if (dayNumID == 0) {
-		timer1 = setTimeout(Announce, timeToNextInMS); debug +=	"timer1 = "+timeToNextInMS;
-		timer2 = setTimeout(Announce, timeToNextInMS + 2*msInDay); debug +=	", timer2 = "+(timeToNextInMS+2*msInDay);
-		timer3 = setTimeout(Announce, timeToNextInMS + 4*msInDay); debug +=	", timer3 = "+(timeToNextInMS+4*msInDay);
+		timer1 = setTimeout(Announce, timeToNextInMS); debug1 +=	"timer1 = "+timeToNextInMS;
+		timer2 = setTimeout(Announce, timeToNextInMS + 2*msInDay); debug1 +=	", timer2 = "+(timeToNextInMS+2*msInDay);
+		timer3 = setTimeout(Announce, timeToNextInMS + 4*msInDay); debug1 +=	", timer3 = "+(timeToNextInMS+4*msInDay);
 	}
 	else if (dayNumID == 2) {
-		timer2 = setTimeout(Announce, timeToNextInMS); debug +=	"timer2 = "+timeToNextInMS;
-		timer3 = setTimeout(Announce, timeToNextInMS + 2*msInDay); debug +=	", timer3 = "+(timeToNextInMS+2*msInDay);
-		timer1 = setTimeout(Announce, timeToNextInMS + 5*msInDay); debug +=	", timer1 = "+(timeToNextInMS+5*msInDay); // Skip 1 for WWG sabbath.
+		timer2 = setTimeout(Announce, timeToNextInMS); debug1 +=	"timer2 = "+timeToNextInMS;
+		timer3 = setTimeout(Announce, timeToNextInMS + 2*msInDay); debug1 +=	", timer3 = "+(timeToNextInMS+2*msInDay);
+		timer1 = setTimeout(Announce, timeToNextInMS + 5*msInDay); debug1 +=	", timer1 = "+(timeToNextInMS+5*msInDay); // Skip 1 for WWG sabbath.
 	}
 	else if (dayNumID == 4) {
-		timer3 = setTimeout(Announce, timeToNextInMS); debug +=	"timer3 = "+timeToNextInMS;
-		timer1 = setTimeout(Announce, timeToNextInMS + 3*msInDay); debug +=	", timer1 = "+(timeToNextInMS+3*msInDay); // Skip 1 for WWG sabbath.
-		timer2 = setTimeout(Announce, timeToNextInMS + 5*msInDay); debug +=	", timer2 = "+(timeToNextInMS+5*msInDay);
+		timer3 = setTimeout(Announce, timeToNextInMS); debug1 +=	"timer3 = "+timeToNextInMS;
+		timer1 = setTimeout(Announce, timeToNextInMS + 3*msInDay); debug1 +=	", timer1 = "+(timeToNextInMS+3*msInDay); // Skip 1 for WWG sabbath.
+		timer2 = setTimeout(Announce, timeToNextInMS + 5*msInDay); debug1 +=	", timer2 = "+(timeToNextInMS+5*msInDay);
 	}
 	else {console.log("<> *Error*: Weird day number.");}
-	console.log("<> Timers now: "+debug+"." );
+	console.log("<> Timers now: "+debug1+"." );
 }
 
+function SpamPrevent() { // timerSpamPrevent is up, calling this to note it's okay to send another announcement.
+	okToSend = true;
+}
 
 function FormatDuration(ms) { // Takes milliseconds.
 	let days = Math.floor(ms / msInDay); ms = ms % msInDay; // .floor because there's no integer division operator that I know of.
